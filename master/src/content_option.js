24cc7b64c2d7230811e0959bbe9c41b74e8f2e44
Zinobuuu <gtzaiko@gmail.com>
2024-10-30 00:16:42 +0800

    initial commit

---
+++ master/src/content_option.js
@@ -0,0 +1,120 @@
+const logotext = "RAINIEL";
+const meta = {
+    title: "Rainiel Jamelarin",
+    description: "I'm Rainiel Jamelarin, a Mobile Developer currently Student at Western institute of Technology",
+};
+
+const introdata = {
+    title: "Rainiel Jamelarin",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "Welcome to my portfolio. Feel free to look around or get in touch if you have any questions.",
+    your_img_url: "https://images.unsplash.com/photo-1729670737935-ab85d9473ad6?q=80&w=1760&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+};
+
+const dataabout = {
+    title: "About my self",
+    aboutme: "Hello, I'm Rainiel Jamelarina, a mobile developer. I make a career by writing code, but I strive for greater things every day. ",
+};
+const worktimeline = [{
+        jobtitle: "N/A",
+        where: "N/A",
+        date: "N/A",
+    },
+    {
+        jobtitle: "N/A",
+        where: "N/A",
+        date: "N/A",
+    },
+    {
+        jobtitle: "N/A",
+        where: "N/A",
+        date: "N/A",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "Java",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 60,
+    },
+    {
+        name: "React",
+        value: 50 ,
+    },
+  
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "You may hire me to help you with ui/ux design, or you can just pay me to do it yourself.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "I can create any kind of basic mobile application you want, just be sure to give me a lot of money.",
+    },
+    {
+        title: "Develop website",
+        description: "If you provide me the specifics, I can make whatever type of website you like.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://i.postimg.cc/q7bg7Dqn/Screenshot-2024-10-28-181317.png",
+        description: " ",
+        link: "#",
+    },
+    {
+        img: "https://i.postimg.cc/8CjNZpkJ/Screenshot-2024-10-28-181211.png",
+        description: " ",
+        link: "#",
+    },
+    {
+        img: "https://i.postimg.cc/xdhY7wqs/Screenshot-2024-10-28-180831.png",
+        description: " ",
+        link: "#",
+    },
+
+   
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "jamelarin.rainielcit@gmail.com",
+    YOUR_PHONE: "(+63)9206410993",
+    description: "Can I be of any assistance to you? If you have any questions about me or the portfolio I created, please feel free to email me using the address I gave or use the form . ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Zinobuuu/Rainiel",
+    facebook: "https://www.facebook.com/Saigo.no.odori?mibextid=ZbWKwL",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
